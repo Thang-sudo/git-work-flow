419e90b71b776d558ce0eec6b727bec104c7a4d1
Thang <leminhthang.tnl@gmail.com>
2024-02-19 15:35:13 -0500

    update 2 files

--- update-yaml.py
+++ update-yaml.py
@@ -17,7 +17,6 @@ def update_yaml_file(input_file):
     if dob:
         age = calculate_age(dob)
         data["age"] = age
-        print("calculated age is " + str(data["age"]))
     
     with open(input_file, 'w') as f:
         yaml.dump(data, f)
@@ -64,8 +63,7 @@ if __name__ == "__main__":
         if file.startswith("savedFilters") and (file.endswith(".yaml") or file.endswith(".yml")):
             updated_file = update_yaml_file(file)
             updated_files.append(updated_file)
-    for file in updated_files:
-        print(file)
+    print('\n'.join(updated_files))
     print("::set-output name=downloaded_files::{}".format('\n'.join(updated_files)))
 
 
