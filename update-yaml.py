a0dd2526d6f55d0841745fce4f143ef5dd44b7f3
Thang <leminhthang.tnl@gmail.com>
2024-02-19 12:24:52 -0500

    Add more debug logs

--- update-yaml.py
+++ update-yaml.py
@@ -50,6 +50,8 @@ def get_changed_files(commit_sha):
         if not changed_files:
             print("ERROR: changed files list is empty")
         print(len(changed_files))
+        for file in changed_files:
+            print(file)
         print("get_changed_files returns: " + subprocess.check_output(command).decode('utf-8').strip())
         return changed_files
     except subprocess.CalledProcessError as e:
