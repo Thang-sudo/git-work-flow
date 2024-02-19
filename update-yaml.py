3ac5686699fffb7b2ec45e418b456ed97712346f
Thang <leminhthang.tnl@gmail.com>
2024-02-19 11:48:01 -0500

    Remove the the get file list step, but instead, incoporate them in the python script

--- update-yaml.py
+++ update-yaml.py
@@ -1,5 +1,6 @@
 import sys;
 import yaml;
+import subprocess;
 from datetime import datetime;
 
 def calculate_age(dob):
@@ -21,6 +22,35 @@ def update_yaml_file(input_file, output_file):
     with open(output_file, 'w') as f:
         yaml.dump(data, f)
 
+def get_current_commit_sha():
+    command = ["git", "rev-parse", "HEAD"]
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    if result.check_returncode == 0:
+        return result.stdout.strip()
+    else:
+        print("ERROR: Failed to execture git rev-parse command")
+        return None
+
+def get_changed_files(commit_sha):
+    if commit_sha is None:
+        print("ERROR: the commit_sha is None")
+        return []
+    # Run get diff-tree command to get list of changed files
+    command = ["git", "diff-tree", "--no-commit-id", "--name-only", "-r", commit_sha]
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    if result.returncode == 0:
+        changed_files = result.stdout.strip().split('\n')
+        return changed_files
+    else:
+        print("ERROR: Failed to get list of changed yaml files")
+        return []
+
 if __name__ == "__main__":
-    input_file = sys.argv[1]
+    commit_sha = get_current_commit_sha()
+    changed_files = get_changed_files(commit_sha)
+    print("list of changed files: ")
+    for file in changed_files:
+        print(file)
 
